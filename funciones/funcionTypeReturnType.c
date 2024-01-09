b9de904db629f6e56d940f319b7a81f0271d13a2
Misael-GC <misael.gomez1623@gmail.com>
2024-01-09 12:09:03 -0600

    tipos de retorno en funciones

---
+++ funciones/funcionTypeReturnType.c
@@ -0,0 +1,30 @@
+//void no retorna nada: sirve para IOT Hardware
+//funciones sin argumentos y sin valor de retorno
+
+//declaración
+void functionName();
+
+//ejecución
+functionName();
+
+//definición de la lógica de la función
+void functionName(){
+    //logica
+}
+
+//Funciones con argumentos pero sin valores a Retornar
+
+void functionName(float);
+
+functionName(float);
+
+void functionName(float)
+{
+    //logica
+}
+
+//Funciones sin argumentos pero que si tienen valores a retornar
+float functionName();
+
+//Funciones con argumentos con return value
+float functionName(int, float);
