2058ba335b09674f91a918d1f0eb549f05806c66
Misael-GC <misael.gomez1623@gmail.com>
2024-01-08 22:13:07 -0600

    do while

---
+++ do-while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int n = 20;
+int main()
+{
+    do{
+    printf("n es igual a %d\n", n);
+        n = n-1;
+    }
+    while (n > 10);
+
+    printf("hemos salido del bucle porque n es igual o menor que 10");
+    return 0;
+}
+
+
