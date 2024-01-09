04ea093245a543728c4ada0d24a391a6ea0dd446
Misael-GC <misael.gomez1623@gmail.com>
2024-01-09 12:49:59 -0600

    funcion con argumentos y con return y de tipo int

--- funciones/funcionNoArgNoReturn.c
+++ funciones/funcionNoArgNoReturn.c
@@ -4,17 +4,17 @@ int a,b;
 a = 100;
 b = 100;
 
-void add(int a, int b);
+int add(int a, int b);
 
 int main()
 {
-    add(100, 100);
-
+    printf("The result is: %d\n", add(100, 100));
     return 0;
 }
 
-void add(int a, int b){
+int add(int a, int b){
     int add;
     add = a+b;
-    printf("The result is: %d\n", add);
+   // 
+   return add;
 }
